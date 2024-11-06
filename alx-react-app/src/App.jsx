f6ba881f15ab914b7b20a5a4792e3b96a6353b46
Saviour Agbor <saviouragbor1@gmail.com>
2024-11-06 21:13:16 +0100

    Modified App.jsx

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -1,27 +1,22 @@
-import { useState } from "react";
-import "./App.css";
-import Footer from "./components/Footer";
-import Header from "./components/Header";
-import MainContent from "./components/MainContent";
-import UserProfile from "./components/UserProfile";
-import WelcomeMessage from "./components/WelcomeMessage";
+import { useState } from 'react'
+import './App.css'
+import Footer from './components/Footer'
+import Header from './components/Header'
+import MainContent from './components/MainContent'
+import UserProfile from './components/UserProfile'
+import WelcomeMessage from './components/WelcomeMessage'
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [count, setCount] = useState(0)
 
   return (
     <>
       <WelcomeMessage />
       <Header />
       <MainContent />
-      <UserProfile
-        name="Saviour Agbor"
-        age="25"
-        bio="Software developer with a passion for open-source and web technologies"
-      />
       <Footer />
-    </>
-  );
+      <UserProfile name="Alice" age="25" bio="Loves hiking and photography" />    </>
+  )
 }
 
-export default App;
+export default App
