8c1b44e03278d802d20593f3853344dca6e68ffb
Saviour Agbor <saviouragbor1@gmail.com>
2024-11-19 01:06:07 +0100

    Modified App.js

--- alx-react-app-props/src/App.jsx
+++ alx-react-app-props/src/App.jsx
@@ -1,17 +1,16 @@
 import ProfilePage from './profilePage';
 import UserContext from './userContext';
-import React, { useState } from 'react';
 
 
 function App() {
-  const userData = useState({ name: "Jane Doe", email: "jane.doe@example.com" });
+  const userData = { name: "Jane Doe", email: "jane.doe@example.com" };
   
   return (
-  <>
-    <UserContext.Provider value={{userData}}>
-      <ProfilePage />
-    </UserContext.Provider>
-  </>
+    <>
+      <UserContext.Provider value={userData}>
+        <ProfilePage />
+      </UserContext.Provider>
+    </>
   );
 }
 
