722f10467dfb4875e14bb7385f0506975e9c86ac
Saviour Agbor <saviouragbor1@gmail.com>
2024-11-19 01:10:57 +0100

    Modified userDetais.jsx

--- alx-react-app-props/src/userDetails.jsx
+++ alx-react-app-props/src/userDetails.jsx
@@ -5,8 +5,8 @@ function UserDetails() {
   const userData = useContext(UserContext);
   return (
     <div>
-      <p>Name: {userData.name}</p>
-      <p>Email: {userData.email}</p>
+      <p>Name: { userData.name }</p>
+      <p>Email: { userData.email }</p>
     </div>
   );
 }
