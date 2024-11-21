e51513d43275f55cd7832db6a201f9064dbc4184
Saviour Agbor <saviouragbor1@gmail.com>
2024-11-21 16:55:45 +0100

    Modified Home.jsx

--- my-company/src/components/Home.jsx
+++ my-company/src/components/Home.jsx
@@ -1,7 +1,7 @@
 function Home() {
   return (
-    <div style={{ padding: '1px', textAlign: 'center'}}>
-      <h1>Welcome to Our Company</h1>
+    <div style={{ padding: '3em', textAlign: 'center'}}>
+      <h1 style={{ color: "cyan"}}>Welcome to Our Company</h1>
       <p>We are dedicated to delivering excellence in all our services.</p>
     </div>
   );
