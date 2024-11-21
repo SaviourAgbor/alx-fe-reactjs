bd9ee22a957fe0974857002ee149d4006482c73f
Saviour Agbor <saviouragbor1@gmail.com>
2024-11-21 16:56:23 +0100

    Modified Services.jsx

--- my-company/src/components/Services.jsx
+++ my-company/src/components/Services.jsx
@@ -1,8 +1,8 @@
 function Services() {
   return (
     <div style={{ padding: '20px' }}>
-      <h1>Our Services</h1>
-      <ul>
+      <h1 style={{ color: "cyan"}}>Our Services</h1>
+      <ul style={{ listStyleType: "none" }}>
         <li>Technology Consulting</li>
         <li>Market Analysis</li>
         <li>Product Development</li>
