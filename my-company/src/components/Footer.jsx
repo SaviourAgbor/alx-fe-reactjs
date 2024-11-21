0c5cbeb19db1ec1ca6d50332023d938898e8488d
Saviour Agbor <saviouragbor1@gmail.com>
2024-11-21 16:55:08 +0100

    Modified Footer.jsx

--- my-company/src/components/Footer.jsx
+++ my-company/src/components/Footer.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 const Footer = () => {
   return (
-    <footer style={{ backgroundColor: "#333", color: "white", padding: "10px", textAlign: "center" }}>
+    <footer style={{ backgroundColor: "#333", color: "white", padding: "10px", textAlign: "center", marginBottom: "0px" }}>
       <p>&coppy; 2024 alx-fe-reactjs my-company project.</p>
     </footer>
   );
