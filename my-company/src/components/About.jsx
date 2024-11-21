7acdb1ac67cbb03f75d177c09ee015c2221f4efb
Saviour Agbor <saviouragbor1@gmail.com>
2024-11-21 16:53:27 +0100

    Modified About.jsx

--- my-company/src/components/About.jsx
+++ my-company/src/components/About.jsx
@@ -1,7 +1,7 @@
 function About() {
   return (
-    <div style={{ padding: '20px' }}>
-      <h1>About Us</h1>
+    <div style={{ padding: '20px', padding:"4em 5em 12em", maxHeight:"100vh"}}>
+      <h1 style= {{ color: "cyan"}}>About Us</h1>
       <p>Our company has been providing top-notch services since 1990. We specialize in various fields including technology, marketing, and consultancy.</p>
     </div>
   );
