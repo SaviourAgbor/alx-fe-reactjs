b218ab0ec4739c96c674a60a46321dae69193cdb
Saviour Agbor <saviouragbor1@gmail.com>
2024-11-21 18:51:54 +0100

    Modified Navbar.jsx

--- my-company/src/components/Navbar.jsx
+++ my-company/src/components/Navbar.jsx
@@ -3,17 +3,31 @@ import { Link } from "react-router-dom";
 
 const Navbar = () => {
   return (
-    <nav style={{ backgroundColor: "#333", padding: "10px", textAlign: "center" }}>
-      <Link to="/" style={{ color: "white", margin: "0 15px", textDecoration: "none" }}>
+    <nav
+      style={{
+        backgroundColor: "#333",
+        padding: "10px",
+        display: "flex",
+        justifyContent: "space-around",
+      }}>
+      <Link
+        to="/"
+        style={{ color: "white", margin: "0 15px", textDecoration: "none" }}>
         Home
       </Link>
-      <Link to="/about" style={{ color: "white", margin: "0 15px", textDecoration: "none" }}>
+      <Link
+        to="/about"
+        style={{ color: "white", margin: "0 15px", textDecoration: "none" }}>
         About
       </Link>
-      <Link to="/services" style={{ color: "white", margin: "0 15px", textDecoration: "none" }}>
+      <Link
+        to="/services"
+        style={{ color: "white", margin: "0 15px", textDecoration: "none" }}>
         Services
       </Link>
-      <Link to="/contact" style={{ color: "white", margin: "0 15px", textDecoration: "none" }}>
+      <Link
+        to="/contact"
+        style={{ color: "white", margin: "0 15px", textDecoration: "none" }}>
         Contact
       </Link>
     </nav>
