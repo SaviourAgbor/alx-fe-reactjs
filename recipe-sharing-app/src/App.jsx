ef35b7e12cd393ed3b2b83b8ba549619a0dcffc6
Saviour Agbor <saviouragbor1@gmail.com>
2024-11-25 07:11:59 +0100

    Create SearchBar Component

--- recipe-sharing-app/src/App.jsx
+++ recipe-sharing-app/src/App.jsx
@@ -1,16 +1,39 @@
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
-import AddRecipeForm from './components/AddRecipeForm';
-import RecipeList from './components/RecipeList';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import RecipeList from "./components/RecipeList";
+import AddRecipeForm from "./components/AddRecipeForm";
+import RecipeDetails from "./components/RecipeDetails";
+import EditRecipeForm from "./components/EditRecipeForm";
+import SearchBar from "./components/SearchBar";
 
 function App() {
-
   return (
-    <>
-      <AddRecipeForm />
-      <RecipeList />
-    </>
+    <Router>
+      <div className="App">
+        {/* Navigation links */}
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/add">Add Recipe</Link>
+            </li>
+          </ul>
+        </nav>
+
+        {/* Routes */}
+        <SearchBar />
+        <Routes>
+          <Route path="/" element={<RecipeList />} />
+          <Route path="/" element={<RecipeList />} />
+          <Route path="/add" element={<AddRecipeForm />} />
+          <Route path="/recipe/:recipeId" element={<RecipeDetails />} />
+          <Route path="/edit/:recipeId" element={<EditRecipeForm />} />
+        </Routes>
+      </div>
+    </Router>
   );
-};
+}
 
 export default App;
