10e54f9ca5c25f288ea131732320a4056a6253e6
Saviour Agbor <saviouragbor1@gmail.com>
2024-11-25 12:20:03 +0100

    Create RecommendationsList

--- recipe-sharing-app/src/components/RecommendationsList.jsx
+++ recipe-sharing-app/src/components/RecommendationsList.jsx
@@ -1,11 +1,13 @@
+// App.js
 import React from "react";
-import { Link, Route, BrowserRouter as Router, Routes } from "react-router-dom";
-import AddRecipeForm from "./components/AddRecipeForm";
-import EditRecipeForm from "./components/EditRecipeForm";
-import FavoritesList from "./components/FavoritesList";
-import RecipeDetails from "./components/RecipeDetails";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import RecipeList from "./components/RecipeList";
+import AddRecipeForm from "./components/AddRecipeForm";
+import RecipeDetails from "./components/RecipeDetails";
+import EditRecipeForm from "./components/EditRecipeForm";
 import SearchBar from "./components/SearchBar";
+import FavoritesList from "./components/FavoritesList";
+import RecommendationsList from "./components/RecommendationsList";
 
 function App() {
   return (
